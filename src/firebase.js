edfc97a0c55adfa812f00112018b289daa5c90d3
Emmanuel <nwodoemmanuel@gmail.com>
2025-05-29 22:33:58 +0100

    installed firebase and initialized firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,54 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import {
+   createUserWithEmailAndPassword,
+   getAuth, 
+   signInWithEmailAndPassword, 
+   signOut} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAltzIWhasbNZpa17JY8vycnCXrUkAzZtU",
-  authDomain: "studio-m7wh3.firebaseapp.com",
-  projectId: "studio-m7wh3",
-  storageBucket: "studio-m7wh3.firebasestorage.app",
-  messagingSenderId: "379869889947",
-  appId: "1:379869889947:web:0c8f068d7a66fa30c38d3e"
+  apiKey: "AIzaSyBJhJbVO31RNMM-lJ_uFDGRCM5JlYC7zCw",
+  authDomain: "netflix-clone-f4cf0.firebaseapp.com",
+  projectId: "netflix-clone-f4cf0",
+  storageBucket: "netflix-clone-f4cf0.firebasestorage.app",
+  messagingSenderId: "482852727505",
+  appId: "1:482852727505:web:a507b1ec2e0b2cbd1773f0"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+  try {
+  const res =  await createUserWithEmailAndPassword(auth, email, password);
+  const user = res.user;
+  await addDoc(collection(db, "user"), {
+      uid:user.uid,
+      name,
+      authProvider:"local",
+      email,
+  })
+  } catch (error) {
+    console.log(error);
+    alert(error);
+    
+  }
+}
+
+const login = async (email, password) =>{
+  try {
+ await signInWithEmailAndPassword(auth, email, password)
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+}
+
+const logout = async () => {
+  signOut(auth);
+}
+
+export {auth, login, signup, logout}
